48eba4da00ecaf0e83628e44640f95ce5abf3afc
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-12-11 15:55:11 +0800

    6.2

---
+++ part6/unicafe-redux/src/main.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { configureStore } from '@reduxjs/toolkit'
+import reducer from './reducers/reducer'
+import counterReducer from './reducers/reducer'
+
+const store = configureStore({
+  reducer: counterReducer
+})
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={good}>good</button> 
+      <button onClick={ok}>ok</button> 
+      <button onClick={bad}>bad</button>
+      <button onClick={zero}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(() => {
+  console.log(store.getState())
+  renderApp()
+}) // calls renderApp whenever an action is dispatched to the store
